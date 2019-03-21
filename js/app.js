ae884224a06e9e00007a68e8f23829bcf538f70b
MaggiR <Mark.Rothermel-07@web.de>
2019-03-21 21:00:08 +0100

    - fixed rare bug where vertical slider repeatedly appeared and disappeared - Reworked drag 'n drop: added drag icons and allowed drag 'n drop now works for mobile devices - disabled tooltips for touch devices - fixed word break for sensor names with very long words - minor design changes and fixes

--- js/app.js
+++ js/app.js
@@ -361,6 +361,10 @@ function diagramSwitched() {
     setMeasurementToggle(device, sensor, value)
 }
 
+function isTouchDevice(){
+	return true === ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
+}
+
 $(function() {
 	loadModel()
     ensureConnection(_ => {
@@ -374,17 +378,27 @@ $(function() {
 	$(document).on('change', '.diagram-switch input', diagramSwitched)
 	$(document).on('click', '#timespanDropdownList .dropdown-item', timespanChanged)
 	$(document).on('click', '#refreshDropdownList .dropdown-item', refreshChanged)
-	$('#menu-list').sortable({ update: onElementReSorted, axis: 'y' })
-	$('#main-cards').sortable({ update: onElementReSorted,
-		cancel: 'h4',
+	$('#menu-list').sortable({ update: onElementReSorted,
+		axis: 'y',
+		handle : '.drag-icon',
+		cursor: 'move'})
+	$('#main-cards').sortable({
+		update: onElementReSorted,
+		// cancel: 'h4',
 		placeholder: 'card my-3 placeholder',
 		start: function(e, ui){
 			if (ui.item.hasClass('card-big')) ui.placeholder.addClass('card-big');
-			ui.placeholder.outerHeight(ui.item.outerHeight())
-        }})
-	$('[data-toggle="tooltip"]').tooltip({
-		animation: true,
-		delay: { show: 1000, hide: 100 }
-	})
+			ui.placeholder.outerHeight(ui.item.outerHeight())},
+		handle : '.drag-icon',
+		cursor: 'move'})
+
+	// Validate if displaying devise uses touch
+	if(isTouchDevice()===false) {
+		// If not, enable tooltips
+		$('[data-toggle="tooltip"]').tooltip({
+			animation: true,
+			delay: {show: 1000, hide: 100}
+		})
+	}
 	$(window).resize(onWindowResize)
 })
